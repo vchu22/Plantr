c792d2c5bfde600e95c05f1bc237d250e106e84d
Mikaela Sheldt <msheldt@gmail.com>
2019-01-18 12:55:02 -0500

    mikaelas changes

--- models.js
+++ models.js
@@ -1,7 +1,57 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://postgres:root@localhost:5432/plantr', {
+// const db = new Sequelize('postgres://postgres:root@localhost:5432/plantr', {
+//   logging: false,
+// });
+const db = new Sequelize('postgres://localhost:5432/plantr', {
   logging: false,
 });
-// const db = new Sequelize('postgres://localhost:5432/plantr');
+
+const Gardener = db.define("gardener", {
+  name : {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  age: {
+    type: Sequelize.INTEGER
+  }
+})
+
+const Plot = db.define("plot", {
+  size : {
+    type: Sequelize.INTEGER
+  },
+  shaded: {
+    type: Sequelize.BOOLEAN
+  }
+})
+
+const Vegetable = db.define("vegetable", {
+  name : {
+    type: Sequelize.STRING
+  },
+  color: {
+    type: Sequelize.STRING
+  },
+  planted_on: {
+    type: Sequelize.DATE
+  }
+})
+
+Plot.belongsTo(Gardener)
+Gardener.hasOne(Plot)
+
+Vegetable.belongsToMany(Plot, {through: 'vegetable_plot'})
+Plot.belongsToMany(Vegetable, {through: 'vegetable_plot'})
+
+Gardener.belongsTo(Vegetable, {as: "favorite_vegetable"})
+Vegetable.hasMany(Gardener)
+
+
+
+
+
+
+
+
 
 module.exports = db;
