de06786949596020d7659d3b9bb48223be2eb3db
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2019-02-18 23:39:11 -0500

    finished 2nd part

--- models.js
+++ models.js
@@ -1,57 +1,49 @@
 const Sequelize = require('sequelize');
-// const db = new Sequelize('postgres://postgres:root@localhost:5432/plantr', {
-//   logging: false,
-// });
-const db = new Sequelize('postgres://localhost:5432/plantr', {
+const db = new Sequelize('postgres://postgres:root@localhost:5432/plantr', {
   logging: false,
 });
+// const db = new Sequelize('postgres://localhost:5432/plantr', {
+//   logging: false,
+// });
 
-const Gardener = db.define("gardener", {
-  name : {
+const Gardener = db.define('gardener', {
+  name: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
   },
   age: {
-    type: Sequelize.INTEGER
-  }
-})
+    type: Sequelize.INTEGER,
+  },
+});
 
-const Plot = db.define("plot", {
-  size : {
-    type: Sequelize.INTEGER
+const Plot = db.define('plot', {
+  size: {
+    type: Sequelize.INTEGER,
   },
   shaded: {
-    type: Sequelize.BOOLEAN
-  }
-})
+    type: Sequelize.BOOLEAN,
+  },
+});
 
-const Vegetable = db.define("vegetable", {
-  name : {
-    type: Sequelize.STRING
+const Vegetable = db.define('vegetable', {
+  name: {
+    type: Sequelize.STRING,
   },
   color: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
   },
   planted_on: {
-    type: Sequelize.DATE
-  }
-})
+    type: Sequelize.DATE,
+  },
+});
 
-Plot.belongsTo(Gardener)
-Gardener.hasOne(Plot)
+Plot.belongsTo(Gardener);
+Gardener.hasOne(Plot);
 
-Vegetable.belongsToMany(Plot, {through: 'vegetable_plot'})
-Plot.belongsToMany(Vegetable, {through: 'vegetable_plot'})
+Vegetable.belongsToMany(Plot, { through: 'vegetable_plot' });
+Plot.belongsToMany(Vegetable, { through: 'vegetable_plot' });
 
-Gardener.belongsTo(Vegetable, {as: "favorite_vegetable"})
-Vegetable.hasMany(Gardener)
-
-
-
-
-
-
-
-
+Gardener.belongsTo(Vegetable, { as: 'favorite_vegetable' });
+Vegetable.hasMany(Gardener);
 
 module.exports = db;
